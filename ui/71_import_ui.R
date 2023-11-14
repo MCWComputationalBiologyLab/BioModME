1a737433f094d4702f7068cf38f6ad260e4ad0ea
Jwomack7512-bio <78865343+Jwomack7512-bio@users.noreply.github.com>
2023-11-14 12:28:31 -0600

    updated sbml import with limitations and description

--- ui/71_import_ui.R
+++ ui/71_import_ui.R
@@ -29,14 +29,27 @@ TAB_IMPORT <-
           titlePanel(
             title = "Import SBML File",
             panel_id = "test_id",
+            "Our SBML importer was created from custom scripts. There are many 
+            complexities and variations in SBML files. As such, we note that 
+            our importer will fail on some files. We do our best to return 
+            where these importers failed, looking to better improve it in the future.",
+            br(),
+            br(),
             fileInput(
               "file_input_load_sbml",
               "SBML",
               placeholder = " .xml",
               multiple = FALSE,
               accept = c(".xml")
-            )
-          )
+            ),
+            HTML("<h5>Current Known Limitations:</h3>
+         <ul>
+           <li>MathML parser cannot read expressions with root and degree (other keywords may be an issue)</li>
+           <li>Models are rendered as unitless. We do not currently parse UnitDefinitions in SBML markup.</li>
+           <li>While we can read function definitions and use those to build equations, we have not currently incorporated a converter to add the functions to the UI. As such, all functions will load as type 'CUSTOM'.</li>
+         </ul>")
+          ),
+          
         )
       )
     )
