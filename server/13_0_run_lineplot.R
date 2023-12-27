1998bc2b4d0c94b7bd939d5be6eb33d420e95e08
Jwomack7512-bio <78865343+Jwomack7512-bio@users.noreply.github.com>
2023-12-27 10:52:52 -0600

    add parm change to viz

--- server/13_0_run_lineplot.R
+++ server/13_0_run_lineplot.R
@@ -715,6 +715,7 @@ output$plot_param_table <- renderRHandsontable({
     colHeaderWidth = 100,
     stretchH = "all"
   ) %>%
+    hot_col("Name", readOnly = TRUE) %>%
     hot_cols(
       colWidth = c(30, 15, 15, 90),
       manualColumnMove = FALSE,
@@ -737,6 +738,208 @@ output$plot_param_table <- renderRHandsontable({
     hot_validate_numeric(col = 2, min = 0)
 })
 
+observeEvent(input$plot_param_table$changes$changes, {
+  xi  = input$plot_param_table$changes$changes[[1]][[1]]
+  yi  = input$plot_param_table$changes$changes[[1]][[2]]
+  old = input$plot_param_table$changes$changes[[1]][[3]]
+  new = input$plot_param_table$changes$changes[[1]][[4]]
+  
+  # Find parameter name that was changed
+  
+  plotted.table <- rv.PARAMETERS$parameters.df
+  
+  # Apply Filters
+  
+  plotted.table <- plotted.table %>% 
+    filter(!Custom)
+  
+  if (input$parameters_filter_type != "All") {
+    plotted.table <- plotted.table %>%
+      filter(Type == input$parameters_filter_type)
+  }
+  
+  plotted.table <- plotted.table %>%
+    select("Name", "Value", "Unit", "Description")
+  
+  par.name <- unname(unlist(plotted.table[xi+1, 1]))
+  par.id   <- FindId(par.name)
+  
+  if (yi == 0) {
+    # PARAMETER NAME CHANGE
+    rv.PARAMETERS$parameters[[par.id]]$Name <- new
+    
+    rv.LOGS$IO.logs <- RenameVarInVector(old, new, rv.LOGS$IO.logs)
+    
+    names.list <- names(rv.REACTIONS)
+    for (name in names.list) {
+      rv.REACTIONS[[name]] <- 
+        replace_word_recursive(rv.REACTIONS[[name]], old, new)
+      
+      rv.REACTIONS[[name]] <- 
+        replace_latex_variable_recursive(rv.REACTIONS[[name]], 
+                                         Var2Latex(old), 
+                                         Var2Latex(new))
+    }
+    
+    names.list <- names(rv.IO)
+    for (name in names.list) {
+      rv.IO[[name]] <- 
+        replace_word_recursive(rv.IO[[name]], old, new)
+      rv.IO[[name]] <- 
+        replace_latex_variable_recursive(rv.IO[[name]],
+                                         Var2Latex(old), 
+                                         Var2Latex(new))
+    }
+    
+    # If volume change in compartment data structure
+    if (rv.PARAMETERS$parameters[[par.id]]$Type == "Compartment") {
+      # Find which compartment has this volume
+      for (i in seq(length(rv.COMPARTMENTS$compartments))) {
+        # If the volume name == old volume name 
+        if (rv.COMPARTMENTS$compartments[[i]]$Volume == old) {
+          rv.COMPARTMENTS$compartments[[i]]$Volume = new
+          break
+        }
+      }
+    }
+    
+    # Change parameter name in ID database
+    idx.for.id <- which(rv.ID$id.df[, 2] %in% old)
+    var.id <- rv.ID$id.df[idx.for.id, 1]
+    rv.ID$id.df[idx.for.id, 2] <- new
+    
+  } 
+  else if (yi == 1) {
+    # PARAMETER VALUE CHANGE
+    # browser()
+    # Set booleans
+    conversion.needed <- FALSE
+    
+    # Parameter value change 
+    rv.PARAMETERS$parameters[[par.id]]$Value <- new
+    
+    # Change base value of parameter if needed
+    selected.unit <- rv.PARAMETERS$parameters[[par.id]]$Unit
+    base.unit     <- rv.PARAMETERS$parameters[[par.id]]$BaseUnit
+    
+    if (is.na(selected.unit) || is.na(base.unit)) {
+      # Account for parameters with no units
+      rv.PARAMETERS$parameters[[par.id]]$BaseValue <- new
+      
+    } else {
+      if (selected.unit != base.unit) {
+        # Perform unit conversion
+        conversion.needed <- TRUE
+        descriptor <- rv.PARAMETERS$parameters[[par.id]]$UnitDescription
+        converted.value <- UnitConversion(descriptor,
+                                          selected.unit,
+                                          base.unit,
+                                          as.numeric(new))
+        rv.PARAMETERS$parameters[[par.id]]$BaseValue <- converted.value
+      } else {
+        rv.PARAMETERS$parameters[[par.id]]$BaseValue <- new
+      }
+      
+      # If volume change in compartment data structure
+      if (rv.PARAMETERS$parameters[[par.id]]$Type == "Compartment") {
+        # Find which compartment has this volume
+        vol.name <- rv.PARAMETERS$parameters[[par.id]]$Name
+        for (i in seq(length(rv.COMPARTMENTS$compartments))) {
+          if (rv.COMPARTMENTS$compartments[[i]]$Volume == vol.name) {
+            if (conversion.needed) {
+              rv.COMPARTMENTS$compartments[[i]]$BaseValue <- converted.value
+            } else {
+              rv.COMPARTMENTS$compartments[[i]]$BaseValue <- new
+            }
+            rv.COMPARTMENTS$compartments[[i]]$Value <- new
+            break
+          }
+        }
+      }
+    }
+    
+    
+  } 
+  else if (yi == 2) {
+    # UNIT CHANGE
+    
+    # Check if no unit exists, then skip and reassign NA
+    # Note Rhandsontable stores NA as NULL, hence the null check
+    if (!is.null(old)) {
+      # check if units are acceptable
+      descriptor <- rv.PARAMETERS$parameters[[par.id]]$UnitDescription
+      
+      # Check to make sure units entered are the right ones
+      comparison <- UnitCompare(descriptor,
+                                new,
+                                rv.UNITS$units.choices)
+      
+      if (comparison$is.match) {
+        # Parameter unit change
+        new <- Unit_Dict_Convert(UNIT_MAPPING, new)
+        
+        rv.PARAMETERS$parameters[[par.id]]$Unit <- new
+        rv.REFRESH$refresh.param.table <- rv.REFRESH$refresh.param.table + 1
+        
+        # We take current value on table as unitvalue
+        # We take current unit as the previous units
+        # We take base unit as new Units
+        # The converted value will be the new base unit value
+        
+        # Perform Conversion for base value if needed
+        from.unit <- rv.PARAMETERS$parameters[[par.id]]$Unit
+        to.unit   <- rv.PARAMETERS$parameters[[par.id]]$BaseUnit
+        from.val  <- rv.PARAMETERS$parameters[[par.id]]$Value
+        
+        if (from.unit != to.unit) {
+          # Perform unit conversion for base
+          descriptor <- rv.PARAMETERS$parameters[[par.id]]$UnitDescription
+          converted.value <- UnitConversion(descriptor,
+                                            from.unit,
+                                            to.unit,
+                                            as.numeric(from.val))
+          rv.PARAMETERS$parameters[[par.id]]$BaseValue <- converted.value
+        } else {
+          rv.PARAMETERS$parameters[[par.id]]$BaseValue <- from.val
+        }
+        
+        # If volume change in compartment data structure change unit there
+        if (rv.PARAMETERS$parameters[[par.id]]$Type == "Compartment") {
+          # Find which compartment has this volume and change unit/basevalue
+          vol.name <- rv.PARAMETERS$parameters[[par.id]]$Name
+          for (i in seq(length(rv.COMPARTMENTS$compartments))) {
+            if (rv.COMPARTMENTS$compartments[[i]]$Volume == vol.name) {
+              rv.COMPARTMENTS$compartments[[i]]$Unit <- 
+                rv.PARAMETERS$parameters[[par.id]]$Unit
+              
+              rv.COMPARTMENTS$compartments[[i]]$BaseValue <- 
+                rv.PARAMETERS$parameters[[par.id]]$BaseValue
+              break
+            }
+          }
+        }
+      } else {
+        # if unit conversion isn't allowed
+        rv.REFRESH$refresh.param.table <- rv.REFRESH$refresh.param.table + 1
+        rv.PARAMETERS$parameters[[par.id]]$Unit <- old
+        sendSweetAlert(
+          session = session,
+          title = "Error...",
+          text = comparison$message,
+          type = "error"
+        )
+      }
+    } else {
+      # Reassign NA
+      rv.REFRESH$refresh.param.table <- rv.REFRESH$refresh.param.table + 1
+      rv.PARAMETERS$parameters[[par.id]]$Unit <- NA
+    }
+    
+  } else if (yi == 3) {
+    # Parameter description change
+    rv.PARAMETERS$parameters[[par.id]]$Description <- new
+  }
+})
 # This table should mirror the table on the main page of the app
 # In the future we should create a module for it but for now we will
 # do a copy and paste.
